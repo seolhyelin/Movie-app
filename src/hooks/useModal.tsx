321f882dd8415dd0480a5dca923733b5017828a8
seolhyelin <tosiltosilkk@gmail.com>
2022-05-14 16:42:50 +0900

    Add: 즐겨찾기 리스트 불러오기

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -2,21 +2,22 @@ import React, { useState } from 'react'
 
 import { useRecoilValue, useRecoilState } from 'recoil'
 import { movieListState, favoriteState } from 'recoil/movieList'
+import { IMovie } from 'types/search'
 
 const useModal = () => {
   const [isShowing, setIsShowing] = useState(false)
   const movieList = useRecoilValue(movieListState)
   const [favoriteMovieList, setFavoriteMovieList] = useRecoilState(favoriteState)
-  const [currentMovieTitle, setCurrentMovieTitle] = useState<string>('')
+  const [currentMovieTitle, setCurrentMovieTitle] = useState('')
 
   const addFavoriteList = () => {
-    const currentSelectedMovie = movieList.findIndex((movie) => movie.Title === currentMovieTitle)
-    const currentSelectedMovieInfo = movieList[currentSelectedMovie]
-    const updateFavoriteMovieList = favoriteMovieList.concat(currentSelectedMovieInfo)
+    const currentSelectedMovie = movieList.find((movie) => movie.Title === currentMovieTitle) as IMovie
+    const updateFavoriteMovieList = favoriteMovieList.concat(currentSelectedMovie)
     setFavoriteMovieList(updateFavoriteMovieList)
     setIsShowing(!isShowing)
     localStorage.setItem('favoriteMovieList', JSON.stringify(updateFavoriteMovieList))
   }
+
   const handleModalVisible = (e: React.MouseEvent<HTMLButtonElement | HTMLLIElement>) => {
     const movieTitle = (e.currentTarget as HTMLLIElement).dataset.title
     if (movieTitle) {
@@ -24,6 +25,7 @@ const useModal = () => {
     }
     setIsShowing(!isShowing)
   }
+
   return { isShowing, handleModalVisible, addFavoriteList }
 }
 export default useModal
--- src/routes/FavoritePage/index.tsx
+++ src/routes/FavoritePage/index.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from 'react'
 import styles from './favoritePage.module.scss'
+import { IMovie } from 'types/search'
+import { useMount } from 'react-use'
 
 const FavoritePage = () => {
+  const [favoriteList, setFavoriteList] = useState<IMovie[]>([])
+
+  useMount(() => {
+    const local = JSON.parse(localStorage.getItem('favoriteMovieList') as string)
+    setFavoriteList(local)
+  })
+
   return (
     <div className={styles.container}>
       <p className={styles.favoriteTitle}>내 즐겨찾기</p>
       <section className={styles.favoriteWrapper}>
         <ul>
-          <li>헤헤</li>
+          {favoriteList.map((movie) => {
+            return (
+              <li key={movie.imdbID} className={styles.movieLists}>
+                <section className={styles.movieItem}>
+                  <img alt={movie.Title} width='100px' height='120px' src={`${movie.Poster}`} />
+                  <article>
+                    <p>{movie.Title}</p>
+                    <p>{movie.Year}</p>
+                    <p>{movie.Type}</p>
+                  </article>
+                </section>
+              </li>
+            )
+          })}
         </ul>
       </section>
     </div>
